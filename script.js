e321f77c7bdafe725bffdf5150945a15d71a9f55
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-02 12:01:00 +0500

    Update code

--- script.js
+++ script.js
@@ -15,32 +15,20 @@ function imageMode(color) {
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  // console.log(': ', toggleIcon.children );
   toggleIcon.children[0].textContent = 'Dark Mode';
 
-  // toggleIcon.children[1].classList.remove('fa-sun');
-  // toggleIcon.children[1].classList.add('fa-moon');
   toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
   
-  // image1.src = 'img/undraw_proud_coder_dark.svg';
-  // image2.src = 'img/undraw_feeling_proud_dark.svg';
-  // image3.src = 'img/undraw_conceptual_idea_dark.svg';
   imageMode('dark');
 }
 
 function lightMode() {
   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  // console.log(': ', toggleIcon.children );
   toggleIcon.children[0].textContent = 'Light Mode';
 
-  // toggleIcon.children[1].classList.remove('fa-moon');
-  // toggleIcon.children[1].classList.add('fa-sun');
   toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  
-  // image1.src = 'img/undraw_proud_coder_light.svg';
-  // image2.src = 'img/undraw_feeling_proud_light.svg';
-  // image3.src = 'img/undraw_conceptual_idea_light.svg';
+
   imageMode('light');
 }
 
